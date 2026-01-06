e95621145b95d69fdcee6b987d75362595aa380c
ka <kaku.h9@gmail.com>
2026-01-06 16:06:25 +0000

    mxfs: implemented bitmap init function

--- src/mxfs/bitmap.c
+++ src/mxfs/bitmap.c
@@ -1,1 +1,25 @@
-#include "types.h"
+#include "bitmap.h"
+
+RSTATUS mx_bitmap_init(mx_bitmap* bitmap, ramdisk* disk, uint16_t base, uint64_t length){
+  if(disk == NULL){
+    return -1;
+  }
+  if(disk->blocksize != MX_BLOCKSIZE){
+    return -1;
+  }
+  bitmap->base = base;
+  bitmap->length = length;
+  // initialising the bitmap onto the disk
+  size_t bytes = length / 8;
+  size_t blocks = bytes/MX_BLOCKSIZE;
+  char block_buffer[MX_BLOCKSIZE];
+  memset(block_buffer, MXBITMAP_FREE, MX_BLOCKSIZE);
+  // write the bitmap data to the 
+  unsigned i = 0;
+  for(i = 0; i < blocks; i++){ 
+    if(ramdisk_write(disk, block_buffer, base + i) == -1){
+      return -1;
+    }
+  }
+  return 0;
+}
--- src/mxfs/bitmap.h
+++ src/mxfs/bitmap.h
@@ -5,15 +5,19 @@
 
 #include "ramdisk.h"
 #include "types.h"
+#include "string.h"
+
+#include "mxfs/types.h"
+
+#define MXBITMAP_FREE 0
+#define MXBITMAP_BUSY 1
 
 typedef struct mx_bitmap{
   uint16_t base;      // index of the disk block which contains the base of the bitmap
   uint64_t length;    // num of bits 
 } mx_bitmap;
 
-void mx_bitmap_init(mx_bitmap* bitmap, ramdisk* disk, uint16_t base, uint64_t length){
-
-}
+RSTATUS mx_bitmap_init(mx_bitmap* bitmap, ramdisk* disk, uint16_t base, uint64_t length);
 
 void mx_bitmap_register_inodemap(mx_bitmap* inodebitmap, mx_superblock* superblock);
 void mx_bitmap_register_blockmap(mx_bitmap* inodebitmap, mx_superblock* superblock);
