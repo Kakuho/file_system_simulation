b5f99ad87c9a2f285ce3a54eaba06c340d2cd701
ka <kaku.h9@gmail.com>
2026-01-08 16:38:21 +0000

    mxfs: added stubs for the block bitmap

--- compile_run.sh
+++ compile_run.sh
@@ -3,6 +3,6 @@
 rm main
 
 gcc -g -o main -I src/ src/main.c src/ramdisk.c \
-      src/mxfs/bitmap.c src/mxfs/mxfs.c src/mxfs/inode_bitmap.c
+      src/mxfs/bitmap.c src/mxfs/mxfs.c src/mxfs/inode_bitmap.c src/mxfs/block_bitmap.c
 
 ./main
---
+++ src/mxfs/block_bitmap.h
@@ -0,0 +1,26 @@
+#ifndef MXFS_BLOCK_BITMAP_H
+#define MXFS_BLOCK_BITMAP_H
+
+// manager class for the disk block bitmap
+
+#include <stdint.h>
+
+#include "types.h"
+#include "ramdisk.h"
+
+#include "mxfs.h"
+
+RSTATUS mxfs_block_bitmap_init(mxfs* mxfs, ramdisk* disk, uint16_t base, uint64_t nblocks);
+RSTATUS mxfs_block_bitmap_setup_memory(ramdisk* disk, uint16_t base, uint64_t nblocks);
+RSTATUS mxfs_block_bitmap_register(mxfs* mxfs, uint16_t base, uint64_t nblocks);
+
+size_t mxfs_block_bitmap_nblocks(mxfs* mxfs);
+
+int64_t mxfs_block_bitmap_get_free_index(char* blockbitmap, mx_superblock* superblock);
+int64_t mxfs_block_bitmap_set(ramdisk* disk, mx_superblock* superblock, uint64_t index);
+int64_t mxfs_block_bitmap_clear(ramdisk* disk, mx_superblock* superblock, uint64_t index);
+
+void mxfs_block_bitmap_allocate(ramdisk* disk);
+void mxfs_block_bitmap_deallocate(ramdisk* disk, int64_t index);
+
+#endif
