5c4cfaaddc5fd153acdcf16e724f3e0797cebae5
ka <kaku.h9@gmail.com>
2026-01-10 16:15:05 +0000

    mxfs: inode zone implemented inode  allocation and deallocation

--- src/mxfs/inode_zone.c
+++ src/mxfs/inode_zone.c
@@ -1,1 +1,123 @@
+#include "inode_zone.h"
 
+RSTATUS mxfs_inode_zone_init(mxfs* mxfs, ramdisk* disk){
+  if(mxfs_inode_zone_setup_memory(mxfs, disk) != 0){
+    return -1;
+  }
+  return 0;
+}
+
+RSTATUS mxfs_inode_zone_setup_memory(mxfs* mxfs, ramdisk* disk){
+  if(disk == NULL){
+    return -1;
+  }
+  if(disk->blocksize != MX_BLOCKSIZE){
+    return -1;
+  }
+  size_t ninodes = mxfs->superblock.ninodes;
+  if(ninodes == 0){
+    return 0;
+  }
+  // initialising the inode blocks of the disk
+  size_t bytes = ninodes * sizeof(mx_disk_inode);
+  size_t blocks = bytes/MX_BLOCKSIZE + 1;
+  unsigned izone_base = mxfs->superblock.inode_base;
+  char block_buffer[MX_BLOCKSIZE];
+  memset(block_buffer, 0, MX_BLOCKSIZE);
+  unsigned i = 0;
+  for(i = 0; i < blocks; i++){ 
+    if(ramdisk_write(disk, block_buffer, izone_base + i) == -1){
+      return -1;
+    }
+  }
+  return 0;
+}
+
+RSTATUS mxfs_inode_zone_poison(mxfs* mxfs, ramdisk* disk, char ch){
+  if(mxfs == NULL | disk == NULL){
+    return -1;
+  }
+  if(disk->blocksize != MX_BLOCKSIZE){
+    return -1;
+  }
+  // initialising the inode blocks of the disk
+  size_t nblocks = mxfs_inode_zone_nblocks(mxfs);
+  unsigned izone_base = mxfs->superblock.inode_base;
+  char block_buffer[MX_BLOCKSIZE];
+  memset(block_buffer, ch, MX_BLOCKSIZE);
+  unsigned i = 0;
+  for(i = 0; i < nblocks; i++){ 
+    if(ramdisk_write(disk, block_buffer, izone_base + i) == -1){
+      return -1;
+    }
+  }
+  return 0;
+}
+
+size_t mxfs_inode_zone_nblocks(mxfs* mxfs){
+  size_t ninodes = mxfs->superblock.ninodes;
+  if(ninodes == 0){
+    return 0;
+  }
+  uintptr_t inodezone_base = mxfs->superblock.inode_base;
+  size_t bytes = ninodes * sizeof(mx_disk_inode);
+  size_t blocks = bytes/MX_BLOCKSIZE + 1;
+  return blocks;
+}
+
+void mxfs_inode_zone_index_to_block_offset(
+  mxfs* mxfs, 
+  uint64_t inode_index,
+  uint32_t* block_index, 
+  uint8_t* offset
+){
+  // given an inode index, we need to figure out which data block on the disk it resides, and what its
+  // offset within the data block is
+  assert(inode_index < mxfs->superblock.ninodes);
+  uint64_t global_byte_offset = inode_index * sizeof(mx_disk_inode);
+  *block_index = global_byte_offset/MX_BLOCKSIZE;
+  *offset = global_byte_offset - *block_index * MX_BLOCKSIZE;
+}
+
+int64_t mxfs_inode_zone_set_inode(
+  mxfs* mxfs,
+  ramdisk* disk, 
+  uint64_t index, 
+  mx_disk_inode* inode
+){
+  assert(index < mxfs->superblock.ninodes);
+  uint32_t blockindex;
+  uint8_t offset;
+  mxfs_inode_zone_index_to_block_offset(mxfs, index, &blockindex, &offset);
+  char buffer[MX_BLOCKSIZE];
+  if(ramdisk_read(disk, buffer, blockindex) != 0){
+    return -1;
+  }
+  // now we can perform the init, simply perform a copy of the memory
+  memcpy(&buffer[offset], inode, sizeof(mx_disk_inode));
+  if(ramdisk_write(disk, buffer, blockindex) != 0){
+    return -1;
+  }
+  return 0;
+}
+
+int64_t mxfs_inode_zone_clear_inode(
+  mxfs* mxfs,
+  ramdisk* disk, 
+  uint64_t index
+){
+  assert(index < mxfs->superblock.ninodes);
+  uint32_t blockindex;
+  uint8_t offset;
+  mxfs_inode_zone_index_to_block_offset(mxfs, index, &blockindex, &offset);
+  char buffer[MX_BLOCKSIZE];
+  if(ramdisk_read(disk, buffer, blockindex) != 0){
+    return -1;
+  }
+  // now we can perform the clear, simply perform a copy of the memory
+  memset(&buffer[offset], 0, sizeof(mx_disk_inode));
+  if(ramdisk_write(disk, buffer, blockindex) != 0){
+    return -1;
+  }
+  return 0;
+}
--- src/mxfs/inode_zone.h
+++ src/mxfs/inode_zone.h
@@ -5,15 +5,16 @@
 #include "mxfs.h"
 #include "ramdisk.h"
 
-RSTATUS mxfs_inode_zone_init(mxfs* mxfs, ramdisk* disk, uint16_t base, uint64_t ninodes);
-RSTATUS mxfs_inode_zone_setup_memory(ramdisk* disk, uint16_t base, uint64_t ninodes);
+RSTATUS mxfs_inode_zone_init(mxfs* mxfs, ramdisk* disk);
+RSTATUS mxfs_inode_zone_setup_memory(mxfs* mxfs, ramdisk* disk);
+
+RSTATUS mxfs_inode_zone_poison(mxfs* mxfs, ramdisk* disk, char ch);
 
 size_t mxfs_inode_zone_nblocks(mxfs* mxfs);
 
-void mxfs_inode_zone_index_to_block_offset(uint64_t inode_index, uint32_t* block_index, uint8_t* offset);
+void mxfs_inode_zone_index_to_block_offset(mxfs* mxfs, uint64_t inode_index, uint32_t* block_index, uint8_t* offset);
 
-int64_t mxfs_inode_zone_init_inode(
-    ramdisk* disk, mx_superblock* superblock, uint64_t index, mx_disk_inode* inode);
-int64_t mxfs_inode_zone_clear_inode(ramdisk* disk, mx_superblock* superblock, uint64_t index);
+int64_t mxfs_inode_zone_set_inode(mxfs* mxfs, ramdisk* disk, uint64_t index, mx_disk_inode* inode);
+int64_t mxfs_inode_zone_clear_inode(mxfs* mxfs, ramdisk* disk, uint64_t index);
 
 #endif
