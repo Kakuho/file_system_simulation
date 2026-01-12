c5ae0f597356e1dc37c22c77ec484b591baf2ed4
ka <kaku.h9@gmail.com>
2026-01-12 21:13:53 +0000

    compile_run.sh now compiles with path_indexer.c tu

--- compile_run.sh
+++ compile_run.sh
@@ -2,7 +2,7 @@
 
 rm main
 
-gcc -g -o main -I src/ src/main.c src/ramdisk.c \
+gcc -g -o main -I src/ src/main.c src/ramdisk.c src/path_indexer.c\
       src/mxfs/bitmap.c src/mxfs/mxfs.c src/mxfs/inode_bitmap.c src/mxfs/block_bitmap.c \
       src/mxfs/inode_zone.c src/mxfs/block_zone.c
 
