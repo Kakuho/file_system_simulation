9120914c12bcff466b53acb84f90c96f6cc83729
ka <kaku.h9@gmail.com>
2026-01-12 16:27:39 +0000

    general: implemented path indexer interface

---
+++ src/path_indexer.c
@@ -0,0 +1,100 @@
+#include "path_indexer.h"
+
+static void print_component(component_indicies* indicies, const char* string){
+  int32_t chidx = indicies->start;
+  do{
+    printf("%c", string[chidx]);
+    chidx++;
+  }
+  while(chidx <= indicies->end); 
+}
+
+RSTATUS path_indexer_parse(path_indexer* path_indexer, const char* path){
+  //  for starting, you have 2 special characters, or you can use the alphanumeric
+  //    ./fkadj/fkadsjf/ - start is index 0 and end is index 1, similiar to an alphanumeric start
+  //    /                - start and end is at index 0
+  //
+  //  for ending you can either use / or not
+  //  /hiya/baka          - start = 6 end = 9
+  //  /hiya/baka.txt      - start = 6 end = 13
+  //  /hiya/baka/         - start = 6 end = 10
+  size_t index = 0;
+  size_t path_length = strlen(path);
+  printf("path length: %lu\n", path_length);
+  path_indexer->ncomponents = 0;
+  path_indexer->path = path;
+
+  if(path[0] == '/'){
+    printf("Absolute Path");
+    path_indexer->entries[0].start = 0;
+    path_indexer->entries[0].end = 0;
+    path_indexer->ncomponents++;
+    index++;
+  }
+  while(1){
+    int32_t comp_start;
+    if(path_indexer->ncomponents == 0){
+      comp_start = 0;
+    }
+    else{
+      comp_start = path_indexer->entries[index-1].end + 1;
+    }
+    //printf("next start: %d \n", comp_start);
+    // parse the next component
+    int32_t comp_indexer = comp_start;
+    while(path[comp_indexer] != '/' && comp_indexer < path_length){
+      //printf("comp indexer: %d \n", comp_indexer);
+      comp_indexer++;
+    }
+    //printf("next end: %d \n", comp_indexer);
+    if(comp_indexer == path_length){
+      //printf("comp indexer == path length\n");
+      comp_indexer--;
+      path_indexer->entries[index].start = comp_start;
+      path_indexer->entries[index].end = comp_indexer;
+      path_indexer->ncomponents++;
+      break;
+    }
+    else{
+      path_indexer->entries[index].start = comp_start;
+      path_indexer->entries[index].end = comp_indexer;
+      path_indexer->ncomponents++;
+      index++;
+    }
+    printf("component start: %d\n", path_indexer->entries[index-1].start);
+    printf("component end: %d\n", path_indexer->entries[index-1].end);
+  }
+  return 0;
+}
+
+RSTATUS path_indexer_read_component(path_indexer* path_indexer, unsigned index, char* buffer){
+  assert(index < path_indexer->ncomponents);
+  int32_t start = path_indexer->entries[index].start;
+  int32_t end = path_indexer->entries[index].end;
+  memcpy(buffer, &path_indexer->path[start], end - start);
+  return 0;
+}
+
+RSTATUS path_indexer_print(path_indexer* path_indexer){
+  uint32_t compidx= 0;
+  char comp_buffer[20];
+  while(compidx != path_indexer->ncomponents){
+    printf("Component: %u\n", compidx);
+    component_indicies* indicies = &path_indexer->entries[compidx];
+    print_component(indicies, path_indexer->path);
+    printf("\n");
+    compidx++;
+  }
+  return 0;
+}
+
+RSTATUS path_indexer_print_indicies(path_indexer* path_indexer){
+  uint32_t compidx= 0;
+  while(compidx != path_indexer->ncomponents){
+    printf("Component %u indicies:\n", compidx);
+    component_indicies* indicies = &path_indexer->entries[compidx];
+    printf("start: %d\nend: %d\n", indicies->start, indicies->end);
+    compidx++;
+  }
+  return 0;
+}
---
+++ src/path_indexer.h
@@ -0,0 +1,30 @@
+#ifndef PATH_INDEXER_H
+#define PATH_INDEXER_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "types.h"
+
+typedef struct component_indicies{
+  // invalid start and ends are marked with -1
+  int32_t start;   
+  int32_t end;       // inclusive (with the /)
+} component_indicies;
+
+typedef struct path_indexer{
+  const char* path;
+  struct component_indicies entries[6];
+  uint32_t ncomponents;
+} path_indexer;     
+
+RSTATUS path_indexer_parse(path_indexer* path_indexer, const char* path);
+RSTATUS path_indexer_read_component(path_indexer* path_indexer, unsigned index, char* buffer);
+
+RSTATUS path_indexer_print(path_indexer* path_indexer);
+RSTATUS path_indexer_print_indicies(path_indexer* path_indexer);
+
+#endif
